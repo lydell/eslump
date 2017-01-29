ede6d83c85bf4c430efaf319cce1dd311bca918e
Simon Lydell <simon.lydell@gmail.com>
2017-01-29 02:15:49 +0100

    Add shift-codegen example

---
+++ examples/shift-codegen.js
@@ -0,0 +1,21 @@
+const shiftCodegen = require("shift-codegen");
+const { parseModule, parseScript } = require("shift-parser");
+const testGenerator = require("./generator");
+const random = require("./random");
+
+function generate(code, { sourceType, options }) {
+  const parseFunction = sourceType === "module" ? parseModule : parseScript;
+  const ast = parseFunction(code, { earlyErrors: false });
+
+  const generator = options.minimal
+    ? shiftCodegen.MinimalCodeGen
+    : shiftCodegen.FormattedCodeGen;
+
+  return shiftCodegen.default(ast, new generator());
+}
+
+function generateRandomOptions() {
+  return { minimal: random.bool() };
+}
+
+module.exports = testGenerator(generate, generateRandomOptions);
