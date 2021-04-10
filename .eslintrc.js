ce605632a51be3719286b0c0e52a7274c35a91cb
Simon Lydell <simon.lydell@gmail.com>
2021-04-10 21:19:21 +0200

    Fix Acorn sourceType

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@
 module.exports = {
   root: true,
   extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2018
+  },
   env: {
     es6: true,
     node: true,
--- examples/acorn.js
+++ examples/acorn.js
@@ -3,6 +3,7 @@
 const acorn = require("acorn");
 const testParser = require("./parser");
 
-const parse = (input) => acorn.parse(input, { ecmaVersion: 2020 });
+const parse = (input, options) =>
+  acorn.parse(input, { ...options, ecmaVersion: "latest" });
 
 module.exports = testParser(parse);
